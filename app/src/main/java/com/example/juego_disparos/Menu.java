c0495463d4e8b16cf76c9de755ab1194dca604ce
Serna28 <juan.sernac@upb.edu.co>
2022-04-23 21:37:25 -0500

    Menu
    Creación de menú con algunos botones, falta aun mas cosas

--- app/src/main/java/com/example/juego_disparos/Menu.java
+++ app/src/main/java/com/example/juego_disparos/Menu.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,6 +17,11 @@ public class Menu extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
 
+    TextView Mipuntunaciontxt,uid,correo,nombre,Menutxt;
+
+    Button BtnJugar,BtnPuntuaciones,BtnAcercaDe;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +29,42 @@ public class Menu extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+
+        Mipuntunaciontxt = findViewById(R.id.Mipuntuaciontxt);
+        uid = findViewById(R.id.uid);
+        correo = findViewById(R.id.correo);
+        nombre = findViewById(R.id.nombre);
+        nombre = findViewById(R.id.nombre);
+        Menutxt = findViewById(R.id.Menutxt);
+
+
+        BtnJugar = findViewById(R.id.BtnJugar);
+        BtnPuntuaciones = findViewById(R.id.BtnPuntuaciones);
+        BtnAcercaDe = findViewById(R.id.BtnAcercaDe);
+
+
+        BtnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Menu.this,"Jugar",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        BtnPuntuaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Menu.this,"Puntuaciones",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        BtnAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Menu.this,"Acerca de nosotros",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
     @Override
     protected void onStart()
