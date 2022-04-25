e569463af22a9083a222b743646d162637e1de46
Serna28 <juan.sernac@upb.edu.co>
2022-04-24 21:05:05 -0500

    Fin Juego Y 90% funcionalidades
    Se termino el juego con opcion de vovler al menu o reintentar, falta la opcion d epuntajes, ya se actualiza con el FireBase sin errores

--- app/src/main/java/com/example/juego_disparos/Escenario.java
+++ app/src/main/java/com/example/juego_disparos/Escenario.java
@@ -1,8 +1,10 @@
 package com.example.juego_disparos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.Typeface;
 import android.media.Image;
@@ -14,26 +16,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Escenario extends AppCompatActivity {
 
-    String NOMBRES,UIDS,ZOMBIES;
-    TextView TvContador,TvNombre,TvTiempo;
+    String NOMBRES, UIDS, ZOMBIES;
+    TextView TvContador, TvNombre, TvTiempo;
     ImageView IvZombie;
 
-    TextView TvAncho,TvAlto;
+    TextView TvAncho, TvAlto;
 
-    int contador =0;
+    int contador = 0;
 
-    int Ancho,Alto;
+    int Ancho, Alto;
 
     boolean GameOver = false;
     Dialog miDialog;
 
     Random aleatorio;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference JUGADORES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +62,15 @@ public class Escenario extends AppCompatActivity {
 
         miDialog = new Dialog(Escenario.this);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        JUGADORES = firebaseDatabase.getReference("BaseDatos Jugadores");
 
 
         IvZombie = findViewById(R.id.IvZombie);
 
-        Bundle intent  = getIntent().getExtras();
+        Bundle intent = getIntent().getExtras();
 
         UIDS = intent.getString("UID");
         NOMBRES = intent.getString("NOMBRE");
@@ -73,8 +93,7 @@ public class Escenario extends AppCompatActivity {
 
                 //Cambia la imagen del zombie normal al zombie mongolo
 
-                if (!GameOver)
-                {
+                if (!GameOver) {
 
                     contador++;
                     TvContador.setText(String.valueOf(contador));
@@ -101,13 +120,12 @@ public class Escenario extends AppCompatActivity {
     }
 
     //TAMAÑO pANTALLA
-    private void pantalla()
-    {
+    private void pantalla() {
         Display display = getWindowManager().getDefaultDisplay();
         Point point = new Point();
         display.getSize(point);
 
-        Ancho=point.x;
+        Ancho = point.x;
         Alto = point.y;
 
         String ANCHOS = String.valueOf(Ancho);
@@ -120,15 +138,14 @@ public class Escenario extends AppCompatActivity {
 
     //Mover el zombie en posiciones Random
 
-    private void Movimiento()
-    {
-        int minimo =0;
+    private void Movimiento() {
+        int minimo = 0;
 
         int MaxX = Ancho - IvZombie.getWidth();
         int MaxY = Alto - IvZombie.getHeight();
 
-        int randomX = aleatorio.nextInt(((MaxX - minimo)+1) +  minimo);
-        int randomY = aleatorio.nextInt(((MaxY - minimo)+1) +  minimo);
+        int randomX = aleatorio.nextInt(((MaxX - minimo) + 1) + minimo);
+        int randomY = aleatorio.nextInt(((MaxY - minimo) + 1) + minimo);
 
 
         IvZombie.setX(randomX);
@@ -137,8 +154,7 @@ public class Escenario extends AppCompatActivity {
 
     //Time Out
 
-    private  void TimeOut()
-    {
+    private void TimeOut() {
         //Sacado de https://developer.android.com/reference/android/os/CountDownTimer#java
 
         new CountDownTimer(30000, 1000) {
@@ -146,8 +162,8 @@ public class Escenario extends AppCompatActivity {
             public void onTick(long millisUntilFinished) {
 
                 //añadido
-                long segundosRestantes = millisUntilFinished/1000;
-                TvTiempo.setText(segundosRestantes+ "S");
+                long segundosRestantes = millisUntilFinished / 1000;
+                TvTiempo.setText(segundosRestantes + "S");
             }
 
             //Time Out
@@ -156,17 +172,18 @@ public class Escenario extends AppCompatActivity {
                 TvTiempo.setText("0s");
                 GameOver = true;
                 MensajeGameOver();
+                GuardarResultado( "Zombie",contador);
             }
         }.start();
 
     }
 
+    //Gameover Screen
     private void MensajeGameOver() {
 
 
-        
-        TextView SeAcabotxt,Hasmatadotxt,Numerotxt;
-        Button Reintentar,IrMenu,Puntajes;
+        TextView SeAcabotxt, Hasmatadotxt, Numerotxt;
+        Button Reintentar, IrMenu, Puntajes;
 
         miDialog.setContentView(R.layout.activity_gameover);
 
@@ -181,12 +198,55 @@ public class Escenario extends AppCompatActivity {
         String zombies = String.valueOf(contador);
         Numerotxt.setText(zombies);
 
+        Reintentar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                contador =0;
+
+                miDialog.dismiss();
+                TvContador.setText("0");
+                GameOver= false;
+                TimeOut();
+                Movimiento();
+
+            }
+        });
+
+        IrMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Escenario.this,Menu.class));
+
+            }
+        });
+
+        Puntajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         miDialog.show();
 
 
+    }
 
+    //Guardar Resultados
 
+    private void GuardarResultado(String key, int zombies)
+    {
+        HashMap <String,Object> hashMap = new HashMap<>();
+        hashMap.put(key,zombies);
 
+        JUGADORES.child(user.getUid()).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(Escenario.this, "Puntaje Actulizado", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
     }
