4fe02ef124a0813cbbe727aabc7d9df5acd5edde
Serna28 <juan.sernac@upb.edu.co>
2022-04-24 19:00:11 -0500

    Implementación Del Juego
    Se realizo el boton de juego, faltan pequeños cambios, pero ya se mueve en posiciones aleatorias y ademas tiene un contador funcional, falta la opcion de que el juego termine cuando el contador llegue a 0

--- app/src/main/java/com/example/juego_disparos/Escenario.java
+++ app/src/main/java/com/example/juego_disparos/Escenario.java
@@ -2,19 +2,35 @@ package com.example.juego_disparos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Point;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Escenario extends AppCompatActivity {
 
     String NOMBRES,UIDS,ZOMBIES;
     TextView TvContador,TvNombre,TvTiempo;
     ImageView IvZombie;
+
+    TextView TvAncho,TvAlto;
+
     int contador =0;
+
+    int Ancho,Alto;
+
+    boolean GameOver = false;
+    Dialog miDialog;
+
+    Random aleatorio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,10 @@ public class Escenario extends AppCompatActivity {
         TvContador = findViewById(R.id.TvContador);
         TvNombre = findViewById(R.id.TvNombre);
         TvTiempo = findViewById(R.id.TvTiempo);
+
+        miDialog = new Dialog(Escenario.this);
+        
+
 
         IvZombie = findViewById(R.id.IvZombie);
 
@@ -35,6 +55,13 @@ public class Escenario extends AppCompatActivity {
 
         TvNombre.setText(NOMBRES);
         TvContador.setText(ZOMBIES);
+
+
+        TvAlto = findViewById(R.id.TvAlto);
+        TvAncho = findViewById(R.id.TvAncho);
+
+        pantalla();
+        TimeOut();
 
         //Al Clickear la iamgen
         IvZombie.setOnClickListener(new View.OnClickListener() {
@@ -55,11 +82,73 @@ public class Escenario extends AppCompatActivity {
                     public void run() {
 
                         IvZombie.setImageResource(R.drawable.zombienormal);
+                        Movimiento();
+
 
                     }
                 },500);
 
             }
         });
+
+    }
+
+    //TAMAÑO pANTALLA
+    private void pantalla()
+    {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+
+        Ancho=point.x;
+        Alto = point.y;
+
+        String ANCHOS = String.valueOf(Ancho);
+        String ALTOS = String.valueOf(Alto);
+
+        aleatorio = new Random();
+
+
+    }
+
+    //Mover el zombie en posiciones Random
+
+    private void Movimiento()
+    {
+        int minimo =0;
+
+        int MaxX = Ancho - IvZombie.getWidth();
+        int MaxY = Alto - IvZombie.getHeight();
+
+        int randomX = aleatorio.nextInt(((MaxX - minimo)+1) +  minimo);
+        int randomY = aleatorio.nextInt(((MaxY - minimo)+1) +  minimo);
+
+
+        IvZombie.setX(randomX);
+        IvZombie.setY(randomY);
+    }
+
+    //Time Out
+
+    private  void TimeOut()
+    {
+        //Sacado de https://developer.android.com/reference/android/os/CountDownTimer#java
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+                //añadido
+                long segundosRestantes = millisUntilFinished/1000;
+                TvTiempo.setText(segundosRestantes+ "S");
+            }
+
+            //Time Out
+
+            public void onFinish() {
+                TvTiempo.setText("0s");
+            }
+        }.start();
+
     }
 }
